373f15c6bd2ac0d0ef3e98ab42fcfe7f11552012
UppiliSrinivas <uppilisrinivasanecs@gmail.com>
2025-12-25 12:17:30 +0530

    Folder restructure

--- String/inbuilt-methods/charAt.js
+++
@@ -1,9 +0,0 @@
-function getMiddleChar(str) {
-  const len = str.length;
-  const mid = Math.floor(len / 2);
-  return len % 2 === 0 ? str.charAt(mid - 1) + str.charAt(mid) : str.charAt(mid);
-}
-
-console.log(getMiddleChar("nivas")); // "v"
-console.log(getMiddleChar("hello")); // "l"
-console.log(getMiddleChar("even")); // "ve"
--- String/inbuilt-methods/length.js
+++
@@ -1,7 +0,0 @@
-function getStringLength(str) {
-  return str.length;
-}
-
-console.log(getStringLength("nivas")); // "v"
-console.log(getStringLength("hell")); // "l"
-console.log(getStringLength("eve")); // "ve"
